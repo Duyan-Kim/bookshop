22ae3e11241dd3627f5e308f452615b70d3a93e0
Duyan-Kim <arbolkim@gmail.com>
2020-11-24 17:37:56 +0900

    html scss js api 개선

--- src/api/index.js
+++ src/api/index.js
@@ -1,40 +1,61 @@
-import { API_KEY } from "../constant.js";
+import { API_KEY } from '../constant';
+import axios from 'axios';
 
-const baby = 109;
+const BABY = 109;
 const kid = 110;
 
 export function getBabyBestSeller() {
   return axios({
-    method: "GET",
-    url: "http://book.interpark.com/api/bestSeller.api",
+    method: 'GET',
+    url: 'http://book.interpark.com/api/bestSeller.api',
     params: {
       key: API_KEY,
-      categoryId: baby,
-      output: "json",
-    },
+      categoryId: BABY,
+      output: 'json'
+    }
   });
 }
 
 export function getKidBestSeller() {
   return axios({
-    method: "GET",
-    url: "http://book.interpark.com/api/bestSeller.api",
+    method: 'GET',
+    url: 'http://book.interpark.com/api/bestSeller.api',
     params: {
       key: API_KEY,
       categoryId: kid,
-      output: "json",
-    },
+      output: 'json'
+    }
   });
 }
 
 export function getRecommended() {
   return axios({
-    method: "GET",
-    url: "http://book.interpark.com/api/recommend.api",
+    method: 'GET',
+    url: 'http://book.interpark.com/api/recommend.api',
     params: {
       key: API_KEY,
-      categoryId: 109,
-      output: "json",
-    },
+      categoryId: BABY,
+      output: 'json'
+    }
+  });
+}
+
+export function getRank() {
+  return axios({
+    method: 'GET',
+    url: '../src/data/rentRanking.json',
+    header: 'application/json'
+  });
+}
+
+export function getNewItem() {
+  return axios({
+    method: 'GET',
+    url: 'http://book.interpark.com/api/newBook.api',
+    params: {
+      key: API_KEY,
+      categoryId: BABY,
+      output: 'json'
+    }
   });
 }
