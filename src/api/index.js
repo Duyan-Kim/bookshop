5014b519c56d5df1313f3e4f42f0d5f93d18f1ba
Duyan-Kim <arbolkim@gmail.com>
2020-11-25 19:02:40 +0900

    원서 베스트셀러 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,12 @@
 import { API_KEY } from '../constant';
 import axios from 'axios';
 
+const Kakao_KEY = 'KakaoAK 1193b096ba6b9f1eabda27c0cf04e2d8';
 const BABY = 109;
 const kid = 110;
+//const NYT_KEY = 'huSM1GjyK83CI4bZdJFVgYf1ZBm59m03';
+
+const inputValue = document.getElementById('inputValue');
 
 export function getBabyBestSeller() {
   return axios({
@@ -56,6 +60,61 @@ export function getNewItem() {
       key: API_KEY,
       categoryId: BABY,
       output: 'json'
+    }
+  });
+}
+
+export function getNytPictureBookBestSeller() {
+  return axios({
+    method: 'GET',
+    url:
+      'https://api.nytimes.com/svc/books/v3/lists/current/picture-books.json?api-key=huSM1GjyK83CI4bZdJFVgYf1ZBm59m03'
+    //params: {
+    //  api-key: KEY,
+    //}
+  });
+}
+
+export function getNytSeriesBookBestSeller() {
+  return axios({
+    method: 'GET',
+    url:
+      'https://api.nytimes.com/svc/books/v3/lists/current/series-books.json?api-key=huSM1GjyK83CI4bZdJFVgYf1ZBm59m03'
+    //params: {
+    //  api-key: KEY,
+    //}
+  });
+}
+
+export function getNytMiddleGradeBookBestSeller() {
+  return axios({
+    method: 'GET',
+    url:
+      'https://api.nytimes.com/svc/books/v3/lists/current/childrens-middle-grade-hardcover.json?api-key=huSM1GjyK83CI4bZdJFVgYf1ZBm59m03'
+    //params: {
+    //  api-key: KEY,
+    //}
+  });
+}
+
+export function getNytYoungAdultBookBestSeller() {
+  return axios({
+    method: 'GET',
+    url:
+      'https://api.nytimes.com/svc/books/v3/lists/current/young-adult-hardcover.json?api-key=huSM1GjyK83CI4bZdJFVgYf1ZBm59m03'
+    //params: {
+    //  api-key: KEY,
+    //}
+  });
+}
+
+export function getSearchResult() {
+  return axios({
+    method: 'GET',
+    url: 'https://dapi.kakao.com/v3/search/book?target=title',
+    params: {
+      Authorization: Kakao_KEY,
+      query: inputValue
     }
   });
 }
---
+++ src/search.js
@@ -0,0 +1,5 @@
+//import { getSearchResult } from './api';
+import axios from 'axios';
+import './search.scss';
+
+//getSearchResult().then((res) => {
